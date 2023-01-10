d290f614a3660a8eb4dd4d168031eaf75e532bc1
Frankie Leyva <frankieiii15@comcast.net>
2023-01-09 18:08:54 -0800

    Update about image

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -1,35 +1,12 @@
 import React from "react";
-
-import axios from "axios";
 import { Jumbotron } from "./migration";
+import primarypic from "../../editable-stuff/profile1.jpg"
 
-const pictureLinkRegex = new RegExp(
-  /[(http(s)?):(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
-);
 
 const AboutMe = ({ heading, message, link, imgSize, resume }) => {
-  const [profilePicUrl, setProfilePicUrl] = React.useState("");
-  const [showPic, setShowPic] = React.useState(Boolean(link));
+  const [showPic] = React.useState(Boolean(link));
   // https://stackoverflow.com/questions/55840294/how-to-fix-missing-dependency-warning-when-using-useeffect-react-hook
-  React.useEffect(() => {
-    const handleRequest = async () => {
-      const instaLink = "https://www.instagram.com/";
-      const instaQuery = "/?__a=1";
-      try {
-        const response = await axios.get(instaLink + link + instaQuery);
-        setProfilePicUrl(response.data.graphql.user.profile_pic_url_hd);
-      } catch (error) {
-        setShowPic(false);
-        console.error(error.message);
-      }
-    };
 
-    if (link && !pictureLinkRegex.test(link)) {
-      handleRequest();
-    } else {
-      setProfilePicUrl(link);
-    }
-  }, [link]);
 
 
 
@@ -38,13 +15,20 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
       <div className="container row">
         <div className="col-5 d-none d-lg-block align-self-center">
           {showPic && (
-            <img
+            <div style={{position:'relative'}}><img style={{position:'absolute'}}
               className="border border-secondary rounded-circle"
-              src={profilePicUrl}
+              src={primarypic}
               alt="profilepicture"
               width={imgSize}
               height={imgSize}
             />
+            <img style={{position:''}}
+              className="border border-secondary rounded-circle"
+              src={link}
+              alt="profilepicture"
+              width={imgSize}
+              height={imgSize}
+            /></div>
           )}
         </div>
         <div className={`col-lg-${showPic ? "7" : "12"}`}>
--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -12,12 +12,20 @@ const mainBody = {
   message: "Creative Developer driving innovation through experimentalism.",
   icons: [
     {
+      pre: "fab",
       image: "fa-github",
       url: "https://github.com/MarineNewt",
     },
     {
+      pre: "fab",
       image: "fa-twitter",
       url: "https://www.twitter.com/Newt936",
+    },
+    {
+      pre: "fa",
+      image: "fa-envelope",
+      url: "mailto: frankieiii15@comcast.net",
+
     },
   ],
 };
@@ -25,7 +33,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/me.png"),
+  imageLink: require("../editable-stuff/profile2.png"),
   imageSize: 375,
   message:
     "My name is Frankie Leyva. I'm a 2022 graduate with a degree in Biology from the University of California, Monterey Bay. I specialize in creative thinking as a solutions architect bringing flair to projects with immersive user experiences. I enjoy exploring the unknown, giving life to unique concepts, and expanding my repertoire to overcome all challenges. ",
@@ -53,12 +61,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/me.png"), 
+      img: require("../editable-stuff/profile2.png"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/me.png"), 
+      img: require("../editable-stuff/profile2.png"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
@@ -108,12 +116,12 @@ const experiences = {
   data: [
     {
       role: 'Solidity Developer',// Here Add Company Name
-      companylogo: require("../editable-stuff/me.png"),// Add logo
+      companylogo: require("../editable-stuff/profile2.png"),// Add logo
       date: 'June 2018 – Present',//Add dates
     },
     {
       role: 'Front-End Developer',
-      companylogo: require("../editable-stuff/me.png"),
+      companylogo: require("../editable-stuff/profile2.png"),
       date: 'May 2017 – May 2018',
     },
   ]
