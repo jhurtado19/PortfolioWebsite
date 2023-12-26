3d4419d877da4bab2d9a2db7a8824d3a364df1b0
Jesse V <133170020+jhurtado19@users.noreply.github.com>
2023-12-25 20:47:00 -0800

    nice

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,10 +6,10 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Frankie",
+  firstName: "Jesse",
   middleName: "",
-  lastName: "Leyva",
-  message: "Creative Developer driving innovation through experimentalism.",
+  lastName: "Hurtado",
+  message: "Welcome!",
   icons: [
     {
       pre: "fab",
