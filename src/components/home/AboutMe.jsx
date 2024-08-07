073c48f8ba5568340dcb63d72577d0925a526f24
Jesse V <133170020+jhurtado19@users.noreply.github.com>
2024-08-07 11:17:47 -0700

    Update AboutMe.jsx

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -33,7 +33,7 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
         </div>
         <div className={`col-lg-${showPic ? "7" : "12"}`}>
           <h2 className="display-4 mb-5 text-center">{heading}</h2>
-          <p className="lead text-center">  My name is Jesse Hurtado, and I study the brain with mathematics and data analytics. I graduated from the University of California, Los Angeles in 2023 with a Bacherlor of Science in Neuroscience. If you've ever asked yourself about the orign of your own thoughts, dreams, and memories, then you have seen what questions move us along in the world of studying the mind. My own work has centered around visualizing neural data and building and testing simulated neurons to better understand their dynamics and decode their mysteries. To see how my mind perceives the world through a camera lens , take a look at my photography <a  href="https://jessehurtado.myportfolio.com/welcome" target="_blank" rel="noopener noreferrer"> portfolio</a> here.</p>
+          <p className="lead text-center">  My name is Jesse Hurtado, and I study the brain with mathematics and data analytics. I graduated from the University of California, Los Angeles in 2023 with a Bacherlor of Science in Neuroscience. If you've ever asked yourself about the orign of your own thoughts, dreams, and memories, then you have seen what questions move us along in the world of studying the mind. My own work has centered around visualizing neural data and building and testing simulated neurons to better understand their dynamics and decode their mysteries.</p>
           {resume && (
             <p className="lead text-center">
               <a
